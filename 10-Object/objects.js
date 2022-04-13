b77d4d2696c822a3f8a1a6eb4a5f60c31f8cce1a
linn <linn.htet@outlook.com>
2022-04-14 00:57:33 +0800

    object-2

--- 10-Object/objects.js
+++ 10-Object/objects.js
@@ -24,7 +24,7 @@
 
 /**
  * ------------------------------------------------------------------------------------------
- * 2. Querying properties
+ * 4. Read properties
  * ------------------------------------------------------------------------------------------
  */
 {
@@ -42,4 +42,70 @@
 
     let fieldName = "age";
     console.log("obj[fieldName]:", obj[fieldName]); // dynamically invoke with square bracket
+}
+
+/**
+ * ------------------------------------------------------------------------------------------
+ * 5. Write properties
+ * ------------------------------------------------------------------------------------------
+ */
+{
+    let obj = Object.create({
+        x: 20,
+        y: 30,
+    },{
+        x:{
+            value: 100,
+        },
+    });
+    console.log("obj:", obj); // print 100
+    console.log("obj.x:", obj.x); // print 100
+    console.log("obj.y:", obj.y); // print 30
+    obj.y = "change"; // writing property didn't update the value on prototype, will create new
+    obj["z"] = "New Z Character"; // writing property didn't update the value on prototype, will create new
+    console.log("obj:", obj); // print 100, change
+}
+
+/**
+ * ------------------------------------------------------------------------------------------
+ * 6. Delete properties
+ * ------------------------------------------------------------------------------------------
+ */
+{
+    let obj = {
+        name: "Alice",
+        age: 23,
+    };
+
+    console.log("obj:", obj);
+    delete obj.name; // delete both key and value
+    console.log("obj:", obj);
+}
+
+/**
+ * ------------------------------------------------------------------------------------------
+ * 7. Testing Properties
+ * ------------------------------------------------------------------------------------------
+ */
+{
+    let obj = {
+        x: 10,
+        y: 20,
+    };
+    console.log("x in obj:", "x" in obj); // return true
+    console.log("z in obj:", "z" in obj); // return false
+    console.log("obj hasOwnProperty x?:", obj.hasOwnProperty("x")); // return true
+    console.log("obj propertyIsEnumerable x?:", obj.propertyIsEnumerable("x"));
+}
+
+/**
+ * ------------------------------------------------------------------------------------------
+ * 8. Enumerating Properties
+ * ------------------------------------------------------------------------------------------
+ */
+{
+    for (let p in global) {
+        console.log("Properties:", p, "Values:", global[p]);
+    }
+    console.log("Keys in Global Object:", Object.keys(global));
 }
