366dcb7ac29389e01b98a2feb2da342641fa0bea
linn <linn.htet@outlook.com>
2022-04-14 22:02:01 +0800

    object-3

---
+++ 10-Object/extended-object-literal.js
@@ -0,0 +1,9 @@
+let x = 10;
+let y = 20;
+
+let obj = {
+    x, // x: x
+    y, // y: y
+};
+
+console.log('obj:', obj);
--- 10-Object/objects.js
+++ 10-Object/objects.js
@@ -24,6 +24,62 @@
 
 /**
  * ------------------------------------------------------------------------------------------
+ * 2. Extending Object
+ * ------------------------------------------------------------------------------------------
+ */
+// with for of Loop 
+{
+    let target = {
+        max: 30,
+        min: 5,
+    };
+    let source = {
+        max: 50,
+    };
+
+    for (const key of Object.keys(source)) {
+        target[key] = source[key];
+    }
+    console.log("Target:", target);
+}
+// with Object.assign() Method
+{
+    let target = {
+        max: 30,
+        min: 5,
+    };
+    let source = {
+        max: 50,
+    };
+    let modified = Object.assign(target, source, { something: "Hello World" });
+    console.log("target:", target);
+    console.log("source:", source);
+    console.log("modified:", modified);
+}
+
+/**
+ * ------------------------------------------------------------------------------------------
+ * 3. Serializing Object
+ * ------------------------------------------------------------------------------------------
+ */
+// JSON.stringinfy() Method [ object to string ]
+{
+    let obj = {
+        name: "Alice",
+        age: 23,
+    };
+    let str = JSON.stringify(obj);
+    console.log("str String:", str);
+}
+// JSON.parse() Method [ string to object ]
+{
+    let str = '{"name": "Alice", "age"  :23}';
+    let obj = JSON.parse(str);
+    console.log("obj Object:", obj);
+}
+
+/**
+ * ------------------------------------------------------------------------------------------
  * 4. Read properties
  * ------------------------------------------------------------------------------------------
  */
@@ -103,9 +159,83 @@
  * 8. Enumerating Properties
  * ------------------------------------------------------------------------------------------
  */
+// with for in loop
 {
     for (let p in global) {
         console.log("Properties:", p, "Values:", global[p]);
     }
+
+}
+
+// with Object.keys() Method
+{
     console.log("Keys in Global Object:", Object.keys(global));
+}
+
+// with Object.getOwnPropertyNames() Method
+{
+    console.log("Keys in Global Object:", Object.getOwnPropertyNames(global));
+}
+
+// with Objet.getOwnPropertySymbol() Method
+{
+    console.log("Keys in Global Object:", Object.getOwnPropertySymbols(global));
+}
+
+// with Reflect.ownKeys() Method
+{
+    console.log("Reflect Keys:", Reflect.ownKeys(global));
+}
+
+/**
+ * ------------------------------------------------------------------------------------------
+ * 9. Object Methods
+ * ------------------------------------------------------------------------------------------
+ */
+// toString() Method
+{
+    let obj = {
+        name: "Alice",
+        age: 23,
+        toString: function () { // redefine toStirng() Method that already defined on prototype object
+            return `name: ${this.name} and age: ${this.age}`;
+        },
+    };
+    console.log("Invokde toString():", obj.toString());
+}
+
+// valueOf() Method
+{
+    let obj1 = {
+        name: "Alice",
+        age: 23,
+        valueOf: function () {
+            return this.age;
+        },
+    };
+    let obj2 = {
+        name: "Yoshi",
+        age: 14,
+        valueOf: function () {
+            return this.age;
+        },
+    };
+    console.log("obj1 < obj2:", obj1 < obj2); // print false
+}
+
+// toJSON() Method
+{
+    let obj = {
+        username: "alice",
+        password: "",
+        max: 30,
+        min: 10,
+        toJSON: function() { // when stringify invoke, toJSON function will called, we can pass specific prop
+            return {
+                username: this.username,
+                max: this.max,
+            };
+        },
+    };
+    console.log("stringify:", JSON.stringify(obj));
 }
